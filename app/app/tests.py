5ac942c510144053b060d0236bc1c8b8d04cc947
PallagiJoe <Pallagi.Jozsef@nexius.hu>
2024-11-20 20:01:44 +0100

    refactor: fix indentation for test functions in CalcTests

--- app/app/tests.py
+++ app/app/tests.py
@@ -9,14 +9,12 @@ from app import calc
 class CalcTests(SimpleTestCase):
     """Test the calc module."""
 
-    def test_add_numbers(self):
-        """Test adding numbers together."""
-        res = calc.addNumbers(5, 6)
-
-        self.assertEqual(res, 11)
+def test_add_numbers(self):
+    """Test adding numbers together."""
+    res = calc.addNumbers(5, 6)
+    self.assertEqual(res, 11)
     
-    def test_subtract_numbers(self):
-        """Test subtracting numbers."""
-        res = calc.subtractNumbers(10, 5)
-
-        self.assertEqual(res, 5)
+def test_subtract_numbers(self):
+    """Test subtracting numbers."""
+    res = calc.subtractNumbers(10, 5)
+    self.assertEqual(res, 5)
